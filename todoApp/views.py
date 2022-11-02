9aba72d38c15e805fd532e80a74513fffa98efe5
Albin <albinroy5555@gmail.com>
2022-11-02 21:12:04 +0530

    initial commit

---
+++ todoApp/views.py
@@ -0,0 +1,65 @@
+
+
+# Create your views here.
+from rest_framework.viewsets import ViewSet,ModelViewSet
+from rest_framework.response import Response
+from api.models import Todos
+from api.serializers import TodoSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+
+class TodosView(ViewSet):
+    def list(self,request,*args,**kw):
+        qs=Todos.objects.all()
+        serializer=TodoSerializer(qs,many=True)
+        return Response(data=serializer.data)
+    def create(self,request,*args,**kw):
+        serializer=TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        else:
+            return Response(data=serializer.errors)
+
+    def retrieve(self,request,*args,**kw):
+        id=kw.get("pk")
+        qs=Todos.objects.get(id=id)
+        serializer=TodoSerializer(qs,many=False)
+        return Response(data=serializer.data)
+    
+    def destroy(self,request,*args,**kw):
+        id=kw.get("pk")
+        Todos.objects.get(id=id).delete()
+        return Response(data="deleted")
+
+    def update(self,request,*args,**kw):
+        id=kw.get("pk")
+        object=Todos.objects.get(id=id)
+        serializer=TodoSerializer(data=request.data,instance=object)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        else:
+            return Response(data=serializer.errors)
+
+#localhost:8000/api/v1/todos/pending_todos/
+#get
+#localhost:8000/api/v1/todos/completed_todos/
+#get
+
+
+class TodosModelViews(ModelViewSet):
+    serializer_class=TodoSerializer
+    queryset=Todos.objects.all()
+
+    @action(methods=["GET"],detail=False)
+    def pending_todos(self,request,*args,**kw):
+        qs=Todos.objects.filter(status=False)
+        serializer=TodoSerializer(qs,many=True)
+        return Response(data=serializer.data)
+
+    @action(methods=["GET"],detail=False)
+    def completed_todos(self,request,*args,**kw):
+        qs=Todos.objects.filter(status=True)
+        serializer=TodoSerializer(qs,many=True)
+        return Response(data=serializer.data)
